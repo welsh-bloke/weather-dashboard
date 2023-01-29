b28e1c55f4ded98724f61c3c70cbf392cc39abe9
Mike Davies <mike@netly.co.uk>
2023-01-29 16:12:25 +0000

    displayd current day forecast

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const mainSection = document.querySelector('#main');
 const searchButton = document.querySelector('#search-button');
 const searchInput = document.querySelector('#city-search');
 const weatherCards = document.querySelector('#weather-cards');
+const currentForecast = document.querySelector('#current-forecast');
 
 const convert = (k) => {
   const c = k -273.15;
@@ -32,12 +33,37 @@ mainSection.addEventListener('click', e => {
         const today = list[0];
         const forecast = [list[8], list[16], list[24], list[32], list[39]];
 
+        let d = new Date(list[0].dt_txt);
+        let currentDate = document.createElement('h5');
+        currentDate.append(`${d.getDate()}/${d.getMonth()}/${d.getFullYear()}`);
+        currentForecast.append(currentDate);
+
+        let weatherIcon = document.createElement('img');
+        weatherIcon.className = 'current-weather-icon';
+        weatherIcon.setAttribute('src', `http://openweathermap.org/img/wn/${list[0].weather[0].icon}.png`);
+        currentForecast.append(weatherIcon);
+
+        let temp = document.createElement('p');
+        temp.className = 'temp';
+        temp.append('Temp ' + convert(list[0].main.temp) +' \xB0C');
+        currentForecast.append(temp);
+
+        let wind = document.createElement('p');
+        wind.className = 'wind';
+        wind.append('Wind ' + list[0].wind.speed);
+        currentForecast.append(wind);
+
+        let hum = document.createElement('p');
+        hum.className = 'hum';
+        hum.append('Humidity ' + list[0].main.humidity);
+        currentForecast.append(hum);
+
         for (let i = 0; i < forecast.length; i++) {
           console.log(forecast[i]);
-          const d = new Date(forecast[i].dt_txt);
+          let d = new Date(forecast[i].dt_txt);
 
           const card = document.createElement('div');
-          card.className = 'card';
+          card.classList.add ('card', 'current-forecast');
 
           const cardBody = document.createElement('div');
           cardBody.className = 'card-body';
